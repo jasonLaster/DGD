3039f10a12d02a6b8b863f6219fd4086af09605d
Matthew McNierney <mmcnierney14@gmail.com>
2011-08-26 01:58:11 -0400

    flags page now links to groups' pages that have been flagged

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,5 +10,9 @@ class Group < ActiveRecord::Base
   def self.recently_updated(num)
     Description.all.sort_by { |d| d.updated_at }.map { |d| d.group_id }.map { |id| Group.find(id) }.uniq.take(num)
   end
+  
+  def self.find_by_description_id(id)
+    Group.find(Description.find(id).group_id)
+  end
 
 end
