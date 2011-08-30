27a9039cc6ab5acd53d42abad01c944cd1ce004e
Jason Laster <jason.laster.11@gmail.com>
2011-08-29 22:20:14 -0700

    add dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
 
+  def page_title
+    controller = params[:controller]
+    action = params[:action]
+
+    if controller == "group" && action == "index" then "DGD - Directory"
+    elsif controller == "group" && action == "show" then "DGD - #{@group.name}"
+    else "DGD"
+    end
+    
+  end
+
   def current_user  
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
   end  
