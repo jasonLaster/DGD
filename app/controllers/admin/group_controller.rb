45398cf1c74678f57b49d4ccfcab561be8531594
Jason Laster <Jason.laster.11@gmail.com>
2011-09-08 16:23:04 -0700

    #22 wire up groups admin page

--- app/controllers/admin/group_controller.rb
+++ app/controllers/admin/group_controller.rb
@@ -2,4 +2,31 @@ class Admin::GroupController < AdminController
   def index
     @groups = Group.all[0..25]
   end
+  
+  def update
+    
+    form_groups = params['groups']
+    db_groups = Group.find(form_groups.keys)
+
+    form_groups.each do |group_id, form_group|
+      db_group = db_groups.find {|c| group_id.to_i == c.id}
+      form_group_locked =  form_group['locked'] == "1"
+
+      if form_group_locked != db_group.locked
+        db_group.locked = form_group_locked
+        db_group.save
+      end
+      
+      if form_group['name']!= db_group.name
+        db_group.name = form_group['name']
+        db_group.save
+      end
+      
+    end
+    
+    
+    
+    redirect_to admin_group_index_path
+  end
+
 end
