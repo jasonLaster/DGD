7c772831096fc81a391cba937054bba997dafd61
Jason Laster <Jason.laster.11@gmail.com>
2011-09-13 13:38:15 -0700

    #29 add page_table query

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,4 +1,23 @@
 class Flag < ActiveRecord::Base
   belongs_to :user
   belongs_to :description
+  
+  def page_table
+    
+    sql = 
+      "
+      SELECT
+       g.name as 'group',
+       d.description as description, 
+       count(*) as num_flags
+      FROM flags f
+      Left join descriptions d on d.id = f.description_id
+      Left join groups g on g.id = d.group_id
+      WHERE d.group_id= ?
+      group by f.description_id
+      order by d.group_id
+      "
+    
+  end
+  
 end
