ae12e9db274402c04bee80ebf97c8fd7bcff1223
Jason Laster <Jason.laster.11@gmail.com>
2011-09-25 18:15:48 -0400

    add ability to add categories

--- app/controllers/admin/category_controller.rb
+++ app/controllers/admin/category_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CategoryController < AdminController
   def index
     @categories = Category.all.sort_by(&:category)
+    5.times {@categories << Category.new}
   end
 
   def primary_categories
@@ -9,9 +10,13 @@ class Admin::CategoryController < AdminController
 
   def update
     
-    form_categories = params['categories']
+    
+    
+    form_categories = params['categories']    
+    form_categories, new_categories = form_categories.partition {|c| is_i?(c.first)}.map {|c| Hash[c]}
     db_categories = Category.find(form_categories.keys)
-
+    
+    # update existing categories
     form_categories.each do |category_id, form_category|
       db_category = db_categories.find {|c| category_id.to_i == c.id}
       
@@ -26,8 +31,19 @@ class Admin::CategoryController < AdminController
       end
       
     end
-    
+
+    # new categories
+    new_categories = new_categories.values.map(&:values).map(&:first).select(&:present?)
+    new_categories.each {|category| Category.create(:category =>  category)}
+
     redirect_to admin_category_index_path
+  end
+  
+  protected
+  
+
+  def is_i?(string)
+     !!(string =~ /^[-+]?[0-9]+$/)
   end
   
 end
