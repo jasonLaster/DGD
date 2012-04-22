b7e5a9264f52a09b01a3d8ff62a7a067d92a6a31
Jason Laster <Jason.laster.11@gmail.com>
2012-04-21 17:10:10 -0700

    change migrations

--- db/migrate/20120422000454_create_group_exec_table.rb
+++ db/migrate/20120422000454_create_group_exec_table.rb
@@ -3,6 +3,7 @@ class CreateGroupExecTable < ActiveRecord::Migration
     create_table :group_execs do |t|
       t.integer :user_id
       t.integer :group_id
+      t.boolean :confirmed, :default => false
     end
     add_index(:group_execs, [:user_id, :group_id], :unique => true, :null => false)    
   end
