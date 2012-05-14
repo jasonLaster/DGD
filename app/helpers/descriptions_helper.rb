300eac762bf2d6f79ed1e15814220a56bfe1ffe5
Jason Laster <Jason.laster.11@gmail.com>
2012-05-13 23:47:56 -0400

    fixes #40 (ughh)

--- app/helpers/descriptions_helper.rb
+++ app/helpers/descriptions_helper.rb
@@ -2,7 +2,7 @@ module DescriptionsHelper
   def history_title(description)
     if description.is_a?(Description)
       date = description.created_at.strftime("%b %d at %I:%M%p")
-      name = description.user.is_a?(User) ? description.user.first_name : ""
+      name = description.user.try(:first_name)
       return "#{name} - #{date}"
     end
   end
