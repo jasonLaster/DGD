9008048090b8aaa02095b7db7a99189be70e6e97
Jason Laster <Jason.laster.11@gmail.com>
2012-05-14 19:03:05 -0400

    add num_pages to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -24,5 +24,19 @@ class Category < ActiveRecord::Base
   def self.groups(category)
     Group.includes(:category).where("categories.category LIKE ?", "%#{category}%")
   end
+  
+  
+  def num_pages
+    groups = self.groups.includes(:descriptions)
+    groups.map {|g| g.descriptions.length}.sum
+  end
+  
+  def self.num_added_pages
+    descriptions = Description.includes(:group => :category)
+    descriptions.group_by {|d| d.group.category}.map do |category, ds|
+      num_descriptions = ds.group_by(&:group).keys.length
+      {category => num_descriptions}
+    end
+  end
 
 end
